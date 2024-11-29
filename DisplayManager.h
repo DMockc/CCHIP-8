cf6ebfa90bb896eb02530e4c3125b9b6fceafa15
DMockc <118026464+DMockc@users.noreply.github.com>
2024-11-29 19:46:52 +0100

    Add files via upload

---
+++ DisplayManager.h
@@ -0,0 +1,80 @@
+#pragma once
+#include "../include/SDL.h"
+#include "../include/SDL_ttf.h"
+#include <vector>
+#include <iostream>
+#include <type_traits>
+
+#define WHITE	255, 255, 255, 255
+#define BLACK	0, 0, 0, 255
+
+#define WIDTH	64
+#define HEIGHT	32
+#define FILL_WIDTH	14
+#define FILL_HEIGHT	8
+
+/* PIXEL OFFSET */
+#define OFFSET	16
+
+using uByte = unsigned char;
+
+/* Display Text */
+struct dTEXT
+{
+	dTEXT(const char* content, int x, int y, SDL_Color color, bool blend);
+
+	std::string t_content; //Text Content
+	int x, y;
+	bool active;
+	bool blend;
+	SDL_Color color;
+	SDL_Rect textPlace;
+};
+
+/*
+*	DRW_COORD is a structure that stores the pixel 
+*	coordinates and the state (black or white).
+*/
+struct DRW_COORD
+{
+	DRW_COORD(uByte x, uByte y, bool color);
+
+	uByte x_coord;
+	uByte y_coord;
+	SDL_Color pixel_color;
+}; 
+
+namespace DisplayMatrix
+{
+	bool get(uint64_t i, uint64_t j);
+	void set(uint64_t i, uint64_t j, bool value);
+	extern uint64_t matrix[HEIGHT];
+};
+
+void waitFPS(Uint32 delta, Uint32 desiredDelta);
+
+class DisplayManager
+{
+public:
+	DisplayManager();
+	~DisplayManager();
+	
+	void clearDisplay();
+	void drawMatrix();
+	void writeText(dTEXT& text);
+	void fillTextPlace(dTEXT& text);
+	void updateAlpha(dTEXT& text, bool speedup, bool slowdown);
+	void updateSpeedText(dTEXT& text);
+	void DisplayQuit();
+
+	SDL_Renderer* renderer;
+
+	std::vector<DRW_COORD> coords_buffer;
+private:
+	void loadFont();
+	void clearTextPlace(dTEXT& text);
+
+	SDL_Window* m_window;
+	SDL_Surface* m_text;
+	TTF_Font* m_font;
+};
